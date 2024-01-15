29ad92a248c454a32bd57176e1cc2f491dc8bbf0
Jose Arias <joseariaspantoj@gmail.com>
2024-01-15 10:22:50 -0500

    Jose Chatboot

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -17,7 +17,16 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         // TODO: escribiremos la funcionalidad principal del bot aquí
-        sendTextMessageAsync("Hola programador");
+        if (getMessageText().equals("/start")) {
+            sendTextMessageAsync("Hola _futuro_ *programador*");
+        }
+        if (getMessageText().contains("hola")) {
+            sendTextMessageAsync("Hola, ¿Cual es tu nombre?");
+        }
+        if (getMessageText().contains("Mi nombre es")) {
+            sendTextMessageAsync("Es un placer yo soy *gato* c\uD83D\uDE3A");
+        }
+        
     }
 
     public static void main(String[] args) throws TelegramApiException {
